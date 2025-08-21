5043f882bb3af0c056551a967b6fd1dd03c86bcc
Reymie Gonzaga <reymielgonzaga@iskolarngbayan.pup.edu.ph>
2025-08-21 11:29:45 +0800

    roses

--- cisco.js
+++ cisco.js
@@ -1,6 +1,14 @@
-let height = 150;
-let width = 100;
+let singleRose = 8;
+let singleLily = 10;
+let singleTulip = 2;
+let roses = 70;
+let lilies = 50;
+let tulips = 120;
+let totalRose = singleRose * roses;
+let totalLily = singleLily * lilies;
+let totalTulips = singleTulip * tulips;
 
-console.log(`The dimensions of the rectangle are ${width}x${height}.`);
-console.log(height);
-console.log(width);
+console.log(`Rose - unit price: ${singleRose}, quantity: ${roses}. value: ${totalRose}`);
+console.log(`Lily - unit price: ${singleLily}, quantity: ${lilies}, value: ${totalLily}`);
+console.log(`Tulip - unit price: ${singleTulip}, quantity: ${tulips}, value: ${totalTulips}`)
+console.log(`Total:  ${totalRose + totalLily + totalTulips}`);
